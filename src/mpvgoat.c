f1adf3c904f4a3551fb4407ae56fde319b2b14aa
naolos-regen <naolos.regen@proton.me>
2025-04-20 01:11:37 +0200

    small changes

--- src/mpvgoat.c
+++ src/mpvgoat.c
@@ -1,6 +1,7 @@
 #include "../includes/v2bg.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <dlfcn.h>
 
 t_mpv	create_mpv_handle(void)
 {
@@ -19,17 +20,20 @@ t_mpv	set_mpv_options(t_mpv mpv, t_ctx *ctx)
 {	
 //	if(!ctx) so being able to do that t_mpv must also be allocated ig
 //		return (NULL);
-
+	void *handle = dlopen("libcuda.so", RTLD_LAZY);
+	
 	snprintf(mpv.wid_str, sizeof(mpv.wid_str), "%lu", ctx->win);
-
 	mpv_set_option_string(mpv.mpv, "vo", "gpu-next");
 	mpv_set_option_string(mpv.mpv, "gpu-api", "vulkan");
 	mpv_set_option_string(mpv.mpv, "wid", mpv.wid_str);
-	mpv_set_option_string(mpv.mpv, "hwdec", "auto");
 	mpv_set_option_string(mpv.mpv, "video-sync", "display-resample");
 	mpv_set_option_string(mpv.mpv, "loop", "inf");
-	mpv_set_option_string(mpv.mpv, "idle", "yes");
-	
+	mpv_set_option_string(mpv.mpv, "idle", "yes");	
+	if (handle)
+		mpv_set_option_string(mpv.mpv, "hwdec", "auto");
+	else
+		printf("mpv starting without hardware acceleration, download libcuda.so. Error %s\n", dlerror());
+	dlclose(handle);
 	return (mpv);
 }
 
